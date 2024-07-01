3ba833dfbed7abd705b290689853254e99e52253
William Galleti <william.galleti@gmail.com>
2024-06-30 20:12:15 -0400

    Add list serializer to separate list to create and update

--- api/core/mixins/views.py
+++ api/core/mixins/views.py
@@ -2,8 +2,13 @@ from rest_framework import viewsets
 
 
 class BaseViewSet(viewsets.ModelViewSet):
+    list_serializer_class = None
+
     def list(self, request, *args, **kwargs):
         if bool(self.request.query_params.get("all", False)):
             self.pagination_class = None
+
+        if self.list_serializer_class is not None:
+            self.serializer_class = self.list_serializer_class
 
         return super().list(request, *args, **kwargs)
